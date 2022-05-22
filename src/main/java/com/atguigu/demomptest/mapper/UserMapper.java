1ab83964cda2019afae593db1c2e6d4438715789
xsLeo <2532413887@qq.com>
2022-05-22 18:20:59 +0800

    Mybatis_plus 测试第一次提交代码

---
+++ src/main/java/com/atguigu/demomptest/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.demomptest.mapper;
+
+import com.atguigu.demomptest.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: Leo messi
+ * @Date: 2022/05/22/18:14
+ * @Description:
+ */
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
---
+++ src/test/java/com/atguigu/demomptest/DemomptestApplicationTests.java
@@ -0,0 +1,24 @@
+package com.atguigu.demomptest;
+
+import com.atguigu.demomptest.entity.User;
+import com.atguigu.demomptest.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class DemomptestApplicationTests {
+
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void findAll(){
+        List<User> users = userMapper.selectList(null);
+        System.out.println("users = " + users);
+    }
+}
